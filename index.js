fec91770692b8d3bdd9087d524c1b1772e5e3f63
Chris Cowan <agentme49@gmail.com>
2019-04-11 17:48:10 -0700

    make flow actually check dependencies

---
+++ index.js
@@ -0,0 +1,25 @@
+/* @flow */
+
+// dummy file that imports everything so Flow checks them
+
+import "auto-html";
+import "braincrunch";
+import "closest-ng";
+import "contain-by-screen";
+import "event-listener-with-options";
+import "flow-bin";
+import "kefir";
+import "kefir-bus";
+import "kefir-stopper";
+import "matches-selector-ng";
+import "mock-webstorage";
+import "pdelay";
+import "react";
+import "react-dom";
+import "react-float-anchor";
+import "react-menu-list";
+import "react-save-refs";
+import "redux-action-log";
+import "synchd";
+import "ud";
+import "ud-kefir";
